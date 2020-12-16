0ec8ed813ccfb1d427af51ed253c89fd90abbd30
ShahabGT <azimishahab@gmail.com>
2020-12-16 21:10:04 +0330

    HighscoreFragment converted to kotlin

--- app/src/main/java/ir/ghararemaghzha/game/fragments/HighscoreFragment.kt
+++ app/src/main/java/ir/ghararemaghzha/game/fragments/HighscoreFragment.kt
@@ -9,7 +9,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.textview.MaterialTextView
 import ir.ghararemaghzha.game.R
-import ir.ghararemaghzha.game.adapters.BuyHistoryAdapter
+import ir.ghararemaghzha.game.adapters.HighscoreAdapter
 import ir.ghararemaghzha.game.classes.MySharedPreference
 import ir.ghararemaghzha.game.classes.RetryInterface
 import ir.ghararemaghzha.game.classes.Utils
@@ -22,13 +22,10 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class BuyHistoryFragment : Fragment(R.layout.fragment_buy_history) {
-
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: BuyHistoryAdapter
+class HighscoreFragment : Fragment(R.layout.fragment_highscore) {
     private lateinit var loading: ConstraintLayout
-    private lateinit var empty: ConstraintLayout
-
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: HighscoreAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -36,16 +33,13 @@ class BuyHistoryFragment : Fragment(R.layout.fragment_buy_history) {
     }
 
     private fun init(v: View) {
-        requireActivity().findViewById<MaterialTextView>(R.id.toolbar_title).setText(R.string.buyhistory_title)
-        loading = v.findViewById(R.id.buyhistory_loading)
-        empty = v.findViewById(R.id.buyhistory_empty)
-        recyclerView = v.findViewById(R.id.buyhistory_recycler)
+        requireActivity().findViewById<MaterialTextView>(R.id.toolbar_title).setText(R.string.highscore_title)
+        recyclerView = v.findViewById(R.id.highscore_recycler)
         recyclerView.layoutManager = LinearLayoutManager(context)
+        loading = v.findViewById(R.id.highscore_loading)
         loading.visibility = View.VISIBLE
-
         CoroutineScope(Dispatchers.IO).launch {
             getData()
-
         }
     }
 
@@ -56,22 +50,38 @@ class BuyHistoryFragment : Fragment(R.layout.fragment_buy_history) {
             Utils.logout(requireActivity(), true)
         }
 
-        when (val res = ApiRepository(RemoteDataSource().getApi(NetworkApi::class.java)).getBuyHistory("Bearer $token", number)) {
+        when (val res = ApiRepository(RemoteDataSource().getApi(NetworkApi::class.java)).getHighscoreList("Bearer $token", number)) {
             is Resource.Success -> {
-
-                if (res.value.result == "success" && res.value.message == "all history") {
+                if (res.value.result == "success") {
                     val data = res.value.data
-                    withContext(Dispatchers.Main) {
-                        loading.visibility = View.GONE
-                        adapter = BuyHistoryAdapter(requireContext(), data)
-                        recyclerView.adapter = adapter
+                    val user = res.value.user
+                    var showUser = true
+                    for (i in data) {
+                        if (i.userId == user.userId) {
+                            showUser = false
+                            break
+                        }
                     }
-                } else
                     withContext(Dispatchers.Main) {
+                        adapter = HighscoreAdapter(requireActivity(), data, user, showUser)
+                        recyclerView.adapter = adapter
                         loading.visibility = View.GONE
-                        empty.visibility = View.VISIBLE }
+                    }
 
+                } else {
+                    withContext(Dispatchers.Main) {
+                        Utils.showInternetError(context, object : RetryInterface {
+                            override fun retry() {
+                                CoroutineScope(Dispatchers.IO).launch {
+                                    getData()
+                                }
+                            }
+                        })
+                        Toast.makeText(context, getString(R.string.general_error), Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
+
             is Resource.Failure -> {
 
                 if (res.isNetworkError) {
@@ -95,4 +105,6 @@ class BuyHistoryFragment : Fragment(R.layout.fragment_buy_history) {
             }
         }
     }
+
+
 }
